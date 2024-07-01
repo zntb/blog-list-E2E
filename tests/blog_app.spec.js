679bd0cfc8220a3067cc423e9a437670def1b56d
zntb <zenetibi@yahoo.com>
2024-07-01 08:48:16 +0300

    Add finished exercise 5.19

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,5 +1,5 @@
 const { test, describe, expect, beforeEach } = require('@playwright/test')
-const {loginWith} = require('./helper.js')
+const {loginWith, createBlog} = require('./helper.js')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -35,5 +35,16 @@ describe('Blog app', () => {
       await expect(errorDiv).toHaveCSS('border', '2px solid rgb(255, 0, 0)');
       await expect(page.getByText('Test User logged in')).not.toBeVisible();
     });
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'testuser', 'pass123')
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await createBlog(page, 'Test Blog', 'testuser', 'http://test.com')
+      await expect(page.getByTestId('notification')).toHaveText('A new blog "Test Blog" by testuser added')
+    })   
   })
 })
--- tests/helper.js
+++ tests/helper.js
@@ -5,4 +5,13 @@ const loginWith = async (page, username, password)  => {
     await page.getByRole('button', { name: 'login' }).click()
   }
 
-module.exports = { loginWith }
+  const createBlog = async (page, title, author, url) => {
+    await page.getByRole('button', { name: 'create new blog' }).click()
+    await page.getByTestId('title').fill(title)
+    await page.getByTestId('author').fill(author)
+    await page.getByTestId('url').fill(url)
+    await page.getByRole('button', { name: 'create' }).click()
+    await page.getByTestId('notification').waitFor()
+  }  
+  
+module.exports = { loginWith, createBlog }
